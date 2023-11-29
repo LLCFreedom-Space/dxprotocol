2211dd702d838122a7f77af323f1f03b9067a338
Mykhailo Bondarenko <mcmikius@icloud.com>
2023-11-29 14:15:53 +0200

    fix: add base path

--- build-docc.sh
+++ build-docc.sh
@@ -8,5 +8,5 @@ xcrun xcodebuild docbuild \
 xcrun docc process-archive transform-for-static-hosting \
     "$PWD/.derivedData/Build/Products/Debug-iphonesimulator/DXProtocol.doccarchive" \
     --output-path ".docs" \
-    --hosting-base-path "" # add your repo name later
+    --hosting-base-path "dxprotocol"
 echo '<script>window.location.href += "/documentation/dxprotocol"</script>' > .docs/index.html
